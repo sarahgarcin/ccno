64ab5be85c46f55cbb403d149759899f20447638
Sarah Garcin <contact@sarahgarcin.com>
2019-11-03 15:49:07 +0100

    refonte accueils

---
+++ site/templates/accueils-back.php
@@ -0,0 +1,48 @@
+<?php snippet('header') ?>
+
+
+<?php $mois = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre'];
+  $day = ['lundi','mardi','mercredi','jeudi','vendredi','samedi','dimanche'];
+?>
+
+<div class ="row">
+
+	<?php snippet('left-col') ?>
+	<?php snippet('menu') ?>
+	<main class="small-18 medium-13 medium-push-4 xlarge-push-4 xlarge-13 end columns">
+		
+		<div class="main-content">
+			<h1 class="small-9 medium-7 large-7 orleans">
+				<?= $page->title()->html() ?></h1>
+			<?php snippet('icone-page')?>
+			<div class="text icones-wrapper-text small-18 medium-16 medium-push-2 large-16 large-push-2 xlarge-10">
+				<div class="large-13">
+					<?php echo $page->text()->kirbytext() ?>
+				</div>
+			</div>
+			<div class="list-with-images small-18 medium-16 medium-push-2 large-14 large-push-2 xlarge-8">
+					<ul class="accueils-module">
+						<?php foreach($page->children()->visible() as $child):?>
+							<li>
+								<a href="<?php echo $child->url()?>" title="<?php echo $child->title()?>">
+									
+									<h3><?php echo $child->personne()->html()?></h3>
+									<h2><?php echo $child->title()->html()?></h2>
+								</a>
+								<?php if($child->hasImages()):?>
+									<div class="thumb-wrapper">
+										<img src="<?php echo $child->images()->first()->url()?>" alt="<?php echo $child->title()?>">
+									</div>
+								<?php endif ?>
+							</li>
+						<?php endforeach?>
+					</ul>
+				</div>
+		</div>
+	</main>
+	
+</div>
+
+
+<?php snippet('footer') ?>
+
--- site/templates/accueils.php
+++ site/templates/accueils.php
@@ -1,48 +1,46 @@
 <?php snippet('header') ?>
 
 
-<?php $mois = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre'];
-  $day = ['lundi','mardi','mercredi','jeudi','vendredi','samedi','dimanche'];
-?>
-
-<div class ="row">
-
-	<?php snippet('left-col') ?>
+<div>
 	<?php snippet('menu') ?>
-	<main class="small-18 medium-13 medium-push-4 xlarge-push-4 xlarge-13 end columns">
-		
-		<div class="main-content">
-			<h1 class="small-9 medium-7 large-7 orleans">
-				<?= $page->title()->html() ?></h1>
+
+	<main class="row">
+		<?php snippet('left-col') ?>
+		<div class="accueils_main col-xs-12 col-sm-11 col-sm-offset-1 col-md-9 col-md-offset-1">
+			<div class="main-content">
+				<h1 class="main-content_title orleans col-xs-12">
+				<?= $page->title()->html() ?>
+				</h1>
 			<?php snippet('icone-page')?>
-			<div class="text icones-wrapper-text small-18 medium-16 medium-push-2 large-16 large-push-2 xlarge-10">
+			<div class="text col-xs-12 col-md-7 col-md-offset-1">
 				<div class="large-13">
 					<?php echo $page->text()->kirbytext() ?>
 				</div>
 			</div>
-			<div class="list-with-images small-18 medium-16 medium-push-2 large-14 large-push-2 xlarge-8">
+			<div class="list-with-images col-xs-12 col-md-10 col-md-offset-1">
 					<ul class="accueils-module">
 						<?php foreach($page->children()->visible() as $child):?>
 							<li>
 								<a href="<?php echo $child->url()?>" title="<?php echo $child->title()?>">
-									
 									<h3><?php echo $child->personne()->html()?></h3>
 									<h2><?php echo $child->title()->html()?></h2>
+									<?php if($child->hasImages()):?>
+										<div class="thumb-wrapper">
+											<figure>
+												<img src="<?php echo $child->images()->last()->url()?>" alt="<?php echo $child->title()?>">
+											</figure>
+										</div>
+									<?php endif ?>
 								</a>
-								<?php if($child->hasImages()):?>
-									<div class="thumb-wrapper">
-										<img src="<?php echo $child->images()->first()->url()?>" alt="<?php echo $child->title()?>">
-									</div>
-								<?php endif ?>
 							</li>
 						<?php endforeach?>
 					</ul>
 				</div>
+			</div>
 		</div>
 	</main>
-	
-</div>
 
+</div>
 
 <?php snippet('footer') ?>
 
